b3fa824b260ea6e44c839c61e7c4c2269eba2e8d
Timothée Ravier <timothee.ravier@ensi-bourges.fr>
2011-01-11 00:11:31 +0100

    Revert "Pas encore teste, ajoute les locks pour verifier les acces, implemente les appels systeme (il manque encore quelques trucs), modifie le code de audit_security_file_permission pour prendre en compte la nouvelle logique"
    This reverts commit 258ede167d6b835a638b63a6d850fc9f401067f1.

--- linux-2.6.32-hardened-r9/security/audit/hooks.h
+++ linux-2.6.32-hardened-r9/security/audit/hooks.h
@@ -1,45 +1,31 @@
 /*
- * Defines structs and static values for system calls
+ *
  **/
-
-
 #include <linux/pid.h>
-#include <linux/limits.h>
-#include <linux/spinlock.h>
 
+static bool auth_started = false;
+static pid_t daemon_pid = 0;
 
 enum ausec_type { AUSEC_FILE, AUSEC_SOCKET};
 
 struct ausec_file {
-	pid_t		pid;
-	char[NAME_MAX + PATH_MAX] full_path;
-	char[1024]	execname;
-	char[1024]	sc;
+	pid_t	pid;
+	char*	full_path;
+	char*	sc;
 };
 
 struct ausec_socket {
-	pid_t		pid;
-	char[1024]	ip_dest;
-	int			port;
+	pid_t	pid;
+	char*	ip_dest;
+	int		port;
 };
 
 struct ausec_info {
-	enum		ausec_type type;
+	enum ausec_type type;
 	union
 	{
-		struct	ausec_file;
-		struct	ausec_socket;
+		struct ausec_file;
+		struct ausec_socket;
 	} ausec_struct;
 };
-
-
-static pid_t		daemon_pid = 0;
-static ausec_info	kernel_ausec_info;
-static const		ausec_info_len = sizeof(struct ausec_info);
-static int			ausec_answer;
-
-static spinlock_t	ausec_anwser_lock = SPIN_LOCK_UNLOCKED;
-static spinlock_t	ausec_hook_lock = SPIN_LOCK_UNLOCKED;
-static spinlock_t	ausec_io_lock = SPIN_LOCK_UNLOCKED;
-static spinlock_t	ausec_auth_lock = SPIN_LOCK_UNLOCKED;
 
