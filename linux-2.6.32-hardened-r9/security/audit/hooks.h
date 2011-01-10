a5a8142d2cea5bebfc0d31e42e61d8a60f2b78b9
Timothée Ravier <timothee.ravier@ensi-bourges.fr>
2011-01-11 00:11:22 +0100

    Revert "corrections mineures, NE COMPILE PAS"
    This reverts commit e320e1db1d743331c03c4b45c38830fe3bc05cd1.

--- linux-2.6.32-hardened-r9/security/audit/hooks.h
+++ linux-2.6.32-hardened-r9/security/audit/hooks.h
@@ -7,12 +7,12 @@
 #include <linux/limits.h>
 #include <linux/spinlock.h>
 
-static int full_path_len = NAME_MAX + PATH_MAX;
-enum ausec_type {AUSEC_FILE, AUSEC_SOCKET};
+
+enum ausec_type { AUSEC_FILE, AUSEC_SOCKET};
 
 struct ausec_file {
 	pid_t		pid;
-	char[full_path_len] full_path;
+	char[NAME_MAX + PATH_MAX] full_path;
 	char[1024]	execname;
 	char[1024]	sc;
 };
@@ -34,11 +34,11 @@ struct ausec_info {
 
 
 static pid_t		daemon_pid = 0;
-static struct ausec_info	kernel_ausec_info;
-static const int	ausec_info_len = sizeof(struct ausec_info);
+static ausec_info	kernel_ausec_info;
+static const		ausec_info_len = sizeof(struct ausec_info);
 static int			ausec_answer;
 
-static spinlock_t	ausec_answer_lock = SPIN_LOCK_UNLOCKED;
+static spinlock_t	ausec_anwser_lock = SPIN_LOCK_UNLOCKED;
 static spinlock_t	ausec_hook_lock = SPIN_LOCK_UNLOCKED;
 static spinlock_t	ausec_io_lock = SPIN_LOCK_UNLOCKED;
 static spinlock_t	ausec_auth_lock = SPIN_LOCK_UNLOCKED;
