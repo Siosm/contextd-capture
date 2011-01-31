acfbbe0d844b837c620fd9107afea06fc13bf15f
Timothée Ravier <timothee.ravier@ensi-bourges.fr>
2011-01-31 18:13:04 +0100

    compile et go

--- linux-2.6.32-hardened-r9/security/audit/share.c
+++ linux-2.6.32-hardened-r9/security/audit/share.c
@@ -2,55 +2,53 @@
 // #include <asm-generic/uacces.h>
 
 #include "share.h"
-
-
-static pid_t		daemon_pid = -1;
-static struct ausec_info	k_ausec_info;
-static int			ausec_answer;
-
-static DECLARE_MUTEX(ausec_hook_lock);
-static DECLARE_MUTEX(ausec_question_lock);
-static DECLARE_MUTEX(ausec_answer_lock);
-static DECLARE_MUTEX(ausec_auth_lock);
+#include "hooks.h"
 
 
 pid_t * daemon_pid()
 {
+	static pid_t		daemon_pid = -1;
 	return &daemon_pid;
 }
 
 
 struct ausec_info * k_ausec_info()
 {
+	static struct ausec_info	k_ausec_info;
 	return &k_ausec_info;
 }
 
 
 int * ausec_answer()
 {
+	static int			ausec_answer = 1;
 	return &ausec_answer;
 }
 
 
 struct semaphore * ausec_hook_lock()
 {
+	static DECLARE_MUTEX(ausec_hook_lock);
 	return &ausec_hook_lock;
 }
 
 
 struct semaphore * ausec_question_lock()
 {
+	static DECLARE_MUTEX(ausec_question_lock);
 	return &ausec_question_lock;
 }
 
 
 struct semaphore * ausec_answer_lock()
 {
+	static DECLARE_MUTEX(ausec_answer_lock);
 	return &ausec_answer_lock;
 }
 
 
 struct semaphore * ausec_auth_lock()
 {
+	static DECLARE_MUTEX(ausec_auth_lock);
 	return &ausec_auth_lock;
 }
