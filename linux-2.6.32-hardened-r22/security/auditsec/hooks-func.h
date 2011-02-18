c9296a58ffc0cc981a89815c2ce0dcaff4795fa7
Dimitri GRESSIN <dimitri.gressin@ensi-bourges.fr>
2011-02-18 16:41:21 +0100

    arbre de confiance

--- linux-2.6.32-hardened-r22/security/auditsec/hooks-func.h
+++ linux-2.6.32-hardened-r22/security/auditsec/hooks-func.h
@@ -4,12 +4,15 @@
 #include <linux/file.h>
 #include <linux/mount.h>
 #include <linux/nfs_mount.h>
+#include "share.h"
 
 int file_path(struct file *, char *);
 
 int dir_path(struct dentry *, char *);
 
 int calculate_path(struct dentry *, char *, size_t); 
+
+void pid_branch (struct task_struct *); 
 
 //int mount_point (struct file *, char * mnt_point);
 
