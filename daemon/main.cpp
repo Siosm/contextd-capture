586760e011813b527cc235902932dcacd65b6f86
Dimitri GRESSIN <dimitri.gressin@ensi-bourges.fr>
2011-03-24 15:00:08 +0100

    Démon - exec_path

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -2,6 +2,8 @@
 #include <malloc.h>
 #include <signal.h>
 #include <string.h>
+#include <fcntl.h>
+#include <math.h>
 
 #include <libcontext.h>
 
@@ -28,6 +30,27 @@ int keep_going = 1;
 // 	return bool;
 // }
 
+int read_execpath (pid_t pid, path)
+{
+	int fd = 0, read = 0;
+	int size = 10 + ceil(log((double)pid));
+	char proc_file[size];
+
+	snprintf(proc_file, size, "/proc/%s/exe", pid);
+	
+	if ((fd = open(proc_file, O_RDONLY|O_EXLOCK)) < 0) {
+		return 1;
+	}
+	
+	if((read = read(fd, path, PATH_MAX)) <= 0) {
+		close(fd);
+		return 1;
+	}
+
+	close(fd);	
+
+	return read;
+}
 
 void signal_manager(int signal)
 {
@@ -40,7 +63,7 @@ int main(int argc, char* argv[])
 	struct auditsec_info * usai = (struct auditsec_info *) malloc(sizeof(struct auditsec_info));
 	int i = 0;
 	struct sigaction action;
-
+	
 	//TODO Finir de bloquer les signaux
 	memset(&action, 0, sizeof(struct sigaction));
 	action.sa_handler = signal_manager;
