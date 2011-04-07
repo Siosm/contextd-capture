1680a5312eb91608bc9a356915274c6164c34b6c
Timothée Ravier <timothee.ravier@ensi-bourges.fr>
2011-04-07 20:48:07 +0200

    suite test

--- linux-2.6.32-hardened-r22/security/auditsec/auditsec_syscall.c
+++ linux-2.6.32-hardened-r22/security/auditsec/auditsec_syscall.c
@@ -32,28 +32,31 @@
  **/
 asmlinkage long sys_auditsec_reg(int state, pid_t contextd, pid_t cnotify)
 {
-	if(down_write_trylock(auditsec_pid_lock()) == 0)
+/*	if(down_write_trylock(auditsec_pid_lock()) == 0)
 		printk(KERN_INFO "AuditSec: Process %d can't register at the moment", task_pid_nr(current));
-	return -1;
-// 	down_write(auditsec_pid_lock());
+	return -1;*/
+	down_write(auditsec_pid_lock());
 
 	if((state == 1) && (*daemon_pid() == -1)){
 		*daemon_pid() = task_pid_nr(current);
 		*contextd_pid() = contextd;
 		*cnotify_pid() = cnotify;
+		up_write(auditsec_pid_lock());
 		printk(KERN_INFO "AuditSec: Process %d successfully registered",
 				task_pid_nr(current));
 	} else if((state == 0) && (*daemon_pid() == task_pid_nr(current))){
 		*daemon_pid() = -1;
 		*contextd_pid() = -1;
 		*cnotify_pid() = -1;
+		up_write(auditsec_pid_lock());
 		printk(KERN_INFO "AuditSec: Process %d successfully unregistered",
 				task_pid_nr(current));
 	} else {
+		up_write(auditsec_pid_lock());
 		printk(KERN_INFO "AuditSec: Process %d NOT registered ; Current is 	%d",
 				task_pid_nr(current), *daemon_pid());
 	}
-	up_write(auditsec_pid_lock());
+	
 
 	return *daemon_pid();
 }
