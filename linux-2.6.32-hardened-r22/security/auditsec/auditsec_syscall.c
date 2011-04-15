ec19a040b621bf3e07f6ae51b957ff57d3746ac7
Timothée Ravier <timothee.ravier@ensi-bourges.fr>
2011-04-15 16:50:07 +0200

    suite black liste

--- linux-2.6.32-hardened-r22/security/auditsec/auditsec_syscall.c
+++ linux-2.6.32-hardened-r22/security/auditsec/auditsec_syscall.c
@@ -36,37 +36,21 @@ asmlinkage long sys_auditsec_reg(int state)
 			printk(KERN_INFO "AuditSec: According to the kernel, the daemon is already launched", task_pid_nr(current));
 			return 1;
 		}else{
-			printk(KERN_INFO "AuditSec: The daemon is now launched", task_pid_nr(current));
+			printk(KERN_INFO "AuditSec: The daemon is now considered launched", task_pid_nr(current));
 			return 0;
 		}
 	}else if(state == 0){
 		up(auditsec_pid_lock());
-		printk(KERN_INFO "AuditSec: The daemon is now stopped", task_pid_nr(current));
+		printk(KERN_INFO "AuditSec: The daemon is now considered stopped", task_pid_nr(current));
 		return 0;
 	}else{
-		;//FIXME get lock state and return
+		return 0;//FIXME get lock state and return
 	}
 }
 
 
 asmlinkage long sys_auditsec_question(struct auditsec_info * user_as_i)
 {
-	if(down_read_trylock(auditsec_pid_lock()) == 0){
-		printk(KERN_INFO "AuditSec: Process %d can't get the lock at the moment", task_pid_nr(current));
-		return -1;
-	}else if(*daemon_pid() == -1){
-		printk(KERN_INFO "AuditSec: Process %d FAILED to question: NOT registered",
-				task_pid_nr(current));
-		up_read(auditsec_pid_lock());
-		return -1;
-	}else if(*daemon_pid() != task_pid_nr(current)){
-		printk(KERN_INFO "AuditSec: Process %d FAILED to question: NOT registered ; Current is %d",
-				task_pid_nr(current), *daemon_pid());
-		up_read(auditsec_pid_lock());
-		return -1;
-	}
-
-	up_read(auditsec_pid_lock());
 	if(down_interruptible(auditsec_question_lock()) != 0)
 		return -1;
 
@@ -88,20 +72,6 @@ asmlinkage long sys_auditsec_question(struct auditsec_info * user_as_i)
 
 asmlinkage long sys_auditsec_answer(int answer)
 {
-	if(down_read_trylock(auditsec_pid_lock()) == 0){
-		return -1;
-	}else if(*daemon_pid() == -1){
-		printk(KERN_INFO "AuditSec: Process %d FAILED to answer: NOT registered",
-				task_pid_nr(current));
-		up_read(auditsec_pid_lock());
-		return -1;
-	}else if(*daemon_pid() != task_pid_nr(current)){
-		printk(KERN_INFO "AuditSec: Process %d FAILED to answer ; Current is %d", task_pid_nr(current), *daemon_pid());
-		up_read(auditsec_pid_lock());
-		return -1;
-	}
-
-	up_read(auditsec_pid_lock());
 	*auditsec_answer() = answer;
 	up(auditsec_answer_lock());
 
