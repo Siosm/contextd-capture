90a9b464dc13c8bffdf7ca5c604a399a185f6250
Timothée Ravier <timothee.ravier@ensi-bourges.fr>
2011-04-28 15:50:28 +0200

    Fixe finalement la compilation de notify.c avec les différentes versions de libnotify, grâce à CMake

--- piga-systrans/src/context-notify_src/notify.c
+++ piga-systrans/src/context-notify_src/notify.c
@@ -22,7 +22,7 @@ int sendNotification(const char* title, const char* message, int timeout, int pr
 		  #ifdef OLD_LIBNOTIFY
 		  notification = notify_notification_new(title, message, img, NULL);
 		  #else
-		  notification = notify_notification_new(title, message, img, NULL);
+		  notification = notify_notification_new(title, message, img);
 		  #endif /* OLD_LIBNOTIFY */
 	  else
 		  notify_notification_update (notification, title, message, img);
